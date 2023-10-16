01c109b0ef31ef2f0210d36c9c1a6064865fa4dc
HUGO SAHUC <hugo@leaf.cloud>
2023-10-16 09:53:06 +0200

    tests

--- exercice_1.py
+++ exercice_1.py
@@ -3,7 +3,8 @@ SPLIT = ' '
 def my_fuction(p_size, dict_possibilities, sentence):
     split_sentence = sentence.split(SPLIT)
     for possibilities in range(p_size):
-        if not split_sentence[possibilities] in dict_possibilities[int(possibilities+1)]:
+        if not int(possibilities+1) in dict_possibilities or \
+            not split_sentence[possibilities] in dict_possibilities[int(possibilities+1)]:
             return False
     return True
 
@@ -21,6 +22,23 @@ dict_possibilities = {1 : ["Je" , "Tu" , "Demain je" ],
        3 : ["." , "!" , "?" , "…" ]}
 sentence_1 = "Je mange !" 
 sentence_2 = "Tu dors ?"
+#test correct
 
 test_my_fuction(p_size, dict_possibilities, sentence_1, True)
 test_my_fuction(p_size, dict_possibilities, sentence_2, False)
+
+#test other wrong dict
+dict_possibilities_wrong = {
+       2 : ["mange" , "bois" , "vole" , "voles" ], 
+       3 : ["." , "!" , "?" , "…" ]}
+
+test_my_fuction(p_size, dict_possibilities_wrong, sentence_1, False)
+
+dict_possibilities_wrong_2 = { 1: [],
+       2 : [], 
+       3 : []}
+test_my_fuction(p_size, dict_possibilities_wrong_2, sentence_1, False)
+
+#test empty
+test_my_fuction(p_size, dict_possibilities, "", False)
+test_my_fuction(0, {}, "", True)
